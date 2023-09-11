388da4f555a6fb003d285d0bb01c246d66ca838f
Nikolay Dobrev <nickndobrev@gmail.com>
2023-09-11 12:35:59 +0300

    Added Arrays exercise

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿//Problem.01
+
+//int[] numbersArr =  Console.ReadLine().Split(' ').Select(int.Parse).ToArray() ;
+
+//for (int i = 0; i < numbersArr.Length / 2; i++)
+//{
+//    int currNum = numbersArr[i];
+//    numbersArr[i] = numbersArr[numbersArr.Length - 1 - i];
+//    numbersArr[numbersArr.Length - 1 - i] = currNum;
+//}
+
+//foreach(int num in numbersArr)  
+//{ 
+//    Console.Write(num + ", ");
+//}
+
+
+int[][] jaggedArr = new int[int.Parse(Console.ReadLine())][];
+for (int i = 0; i < jaggedArr[0].Length; i++)
+{
+
+}
